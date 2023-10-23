e505e21021fcedc9d5d13d26bb026e808e7be531
cooleydw494 <cooleydw494@gmail.com>
2023-10-23 00:59:36 -0400

    install autoprefixer

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  postcss: {
+    plugins: [
+      require('tailwindcss'),
+      require('autoprefixer'),
+    ],
+  },
   pluginOptions: {
     tailwindcss: {
         //
